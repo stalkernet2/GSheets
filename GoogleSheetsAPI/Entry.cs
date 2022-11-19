0b1719f631ac50b168f7f48611aaadafe09a9807
stalkernet2 <stalker.net74rus@gmail.com>
2022-11-19 14:05:46 +0300

    Добавьте файлы проекта.

---
+++ GoogleSheetsAPI/Entry.cs
@@ -0,0 +1,52 @@
+﻿using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Sheets.v4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleSheetsAPI
+{
+    public partial class GSheets
+    {
+        public IList<IList<object>> ReadEntries(string range)
+        {
+            var request = _sheetsService.Spreadsheets.Values.Get(SpreadsheetId, range);
+            var response = request.Execute();
+
+            return response.Values;
+        }
+
+        public void CreateEntry(string range, List<object> objectList)
+        {
+            var valueRange = new ValueRange();
+            valueRange.Values = new List<IList<object>> { objectList };
+
+            var appendRequest = _sheetsService.Spreadsheets.Values.Append(valueRange, SpreadsheetId, range);
+            appendRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+
+            var appendResponce = appendRequest.Execute();
+        }
+
+        public void UpdateEntry(string range, List<object> objectList)
+        {
+            var valueRange = new ValueRange();
+
+            valueRange.Values = new List<IList<object>> { objectList };
+
+            var updateRequest = _sheetsService.Spreadsheets.Values.Update(valueRange, SpreadsheetId, range);
+            updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.USERENTERED;
+            var updateResponce = updateRequest.Execute();
+        }
+
+        public void DeleteEntry(string range)
+        {
+            var requestBody = new ClearValuesRequest();
+
+            var deleteRequest = _sheetsService.Spreadsheets.Values.Clear(requestBody, SpreadsheetId, range);
+            var deleteResponce = deleteRequest.Execute();
+        }
+    }
+}
---
+++ GoogleSheetsAPI/GSheets.cs
@@ -0,0 +1,30 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+
+namespace GoogleSheetsAPI
+{
+    public partial class GSheets
+    {
+        public string SpreadsheetId;
+
+        protected private SheetsService _sheetsService;
+
+        private readonly string[] _scopes = { SheetsService.Scope.Spreadsheets };
+
+        public GSheets(string secretFilePath, string applicationName)
+        {
+            GoogleCredential credential;
+
+            using (var stream = new FileStream(secretFilePath, FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleCredential.FromStream(stream).CreateScoped(_scopes);
+            }
+
+            _sheetsService = new SheetsService(new Google.Apis.Services.BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = applicationName,
+            });
+        }
+    }
+}
